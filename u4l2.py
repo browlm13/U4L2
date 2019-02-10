f5df6bf3205f1353ce1dfb20e8d478bd00b83644
LJ Brown <browlm13@gmail.com>
2019-02-10 16:56:09 -0600

    Create u4l2.py

---
+++ u4l2.py
@@ -0,0 +1,193 @@
+
+#  Actually ...  not too
+#   Qíshí   ...  Bìng bù
+#    其实    ...    并不
+
+# ... actually not ...
+# ...   Bìng bù ...
+
+# (Subj.) 并不 (adj./v.)
+
+
+# lettuce / salad
+#     Shēngcài
+#       生菜
+
+# classmates
+#  Tóngxué
+#    同学
+
+# roomates
+#  Tóngwū
+#    同屋
+
+# Because
+# Yīnwèi
+#   因为
+
+#  Inside
+#    Lǐ
+#    里
+
+#   cup
+#  Bēizi
+#   杯子
+
+# in the cup
+# bēizi lǐ 
+# 杯子里
+
+#  Since / Already
+#      jìrán​
+#       既然
+
+# Then
+# jiù​
+#  就
+
+# Since ..., Then ...
+# jìrán​ ..., jiù​ ...
+# 既然  ..., 就 ...
+
+# Why
+# Wèishéme
+# 为什么
+
+# Since ..., Why ... ?
+# jìrán​ ..., Wèishéme ... ?
+# 既然  ..., 为什么 ... ?
+
+# cut meat
+#  qiēròu 
+#   切肉
+
+#    See
+#  Kànjiàn
+#    看见
+
+#    Hear
+#  Tīngjiàn
+#    听见
+
+# therefore
+#  suǒyǐ 
+# 所以
+
+# suǒ
+# 所
+
+# Tīng
+# 听
+
+# left hand
+#  zuǒshǒu 
+#   左手
+
+# Classroom
+#  Jiàoshì
+#    教室
+
+#  Joke
+# Xiàohuà
+#  笑话
+
+# make a fool of oneself; make dumb mistakes
+# 				nào xiàohuà 
+# 					闹笑话
+
+# Yòng bùyòng
+#   用不用
+
+# Face
+# Liǎn
+#  脸
+
+#  Next To
+# Pángbiān
+#   旁边
+
+#  Easy
+# Róngyì
+#  容易
+
+#  Hard
+#  Nán
+#   难
+
+# not difficult
+# 	bù nán 
+# 	 不难
+
+# Must
+# děi​
+#  得
+
+# Bite
+# Yǎo
+#  咬
+
+# Polite
+# Kèqì
+# 客气
+
+# Resembles / A Like B
+#       xiàng​
+#        像
+
+# Seems Like
+# Hǎoxiàng
+#   好像
+
+# lettuce fell into a cup。
+# shēngcài diào dào bēi.zi.ǐ le。
+# 生菜掉到杯子里了。
+
+#  I saw lettuce fell next to the cup。
+# wǒ kànjiàn shēngcài diào dào bēizi pángbiān le。
+# 我看见生菜掉到杯子旁边了。 
+
+# therefore
+#  suǒyǐ 
+# 所以
+
+# can't understand from listening
+# tīngbùdǒng
+# 听不懂
+
+# ( thought ( mistakenly) but actually...) 
+# yǐwéi qíshí 
+# 以为..., 其实...
+
+# can't get used to ...lifestyle
+# guòbúguàn 
+# 过不惯
+
+# knife and fork
+# dāochā 
+# 刀叉
+
+# use knife and fork
+# yòng dāochā 
+# 用刀叉
+
+# ( she has used knife and fork before) 
+# tā yòng guò dāochā 
+# 她用过刀叉。 
+
+# level
+# shuǐpíng
+# 水平
+
+# stomach
+# dùzi  
+# 肚子
+
+# she eats with hands
+# tā yòng shǒu chīfàn
+# 她用手吃饭。 
+
+# guò
+# 过
+
+# wéi
+# 为
